227ee5430c3a8df384c5116ae789ec9d1238937c
James Halliday <mail@substack.net>
2012-11-06 20:36:20 -0800

    convert to base64 strings for comparison

--- test/expected.js
+++ test/expected.js
@@ -4,6 +4,15 @@ var keys = require('./data/keys.json');
 var unpack = require('../');
 
 test('output matches openssl rsa -text', function (t) {
-    t.same(unpack(keys.private), expected);
+    var c = unpack(keys.private);
+    var b = Object.keys(c).reduce(function (acc, key) {
+        if (Buffer.isBuffer(c)) {
+            acc[key] = c.toString('base64');
+        }
+        else acc[key] = c[key];
+        return acc;
+    }, {});
+    
+    t.same(b, expected);
     t.end();
 });
