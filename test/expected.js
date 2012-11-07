15b97a7659adab43b5390ab6e0a9b17686fb540e
James Halliday <mail@substack.net>
2012-11-06 23:01:40 -0800

    failing test when bits = -Infinity

--- test/expected.js
+++ test/expected.js
@@ -2,16 +2,21 @@ var test = require('tap').test;
 var expected = [
     require('./data/expected0.json'),
     require('./data/expected1.json'),
+    require('./data/expected2.json'),
 ];
 var keys = [
     require('./data/keys0.json'),
-    require('./data/keys1.json')
+    require('./data/keys1.json'),
+    require('./data/keys2.json'),
 ];
 var unpack = require('../');
 
 test('output matches openssl rsa -text', function (t) {
-    t.same(unbuffer(unpack(keys[0].private)), expected[0]);
-    t.same(unbuffer(unpack(keys[1].private)), expected[1]);
+    t.plan(3);
+    
+    keys.forEach(function (key, ix) {
+        t.same(unbuffer(unpack(key.private)), expected[ix]);
+    });
     t.end();
 });
 
